c0a0150e81bf5a08740e53048e83be57f831a98d
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-02-17 19:43:48 -0800

    BLT demo needed some platform stuff; x64 is a must

--- code/Demo/Main.cs
+++ code/Demo/Main.cs
@@ -1,8 +1,17 @@
 ﻿namespace Demo {
     class Demo {
+        // currently, you can press spacebar (or most keys) to display randomly-placed 'a' glyphs, and press Escape to close.
         static void Main(string[] args) {
             BearLib.Terminal.Open();
-            BearLib.Terminal.Refresh();
+            BearLib.Terminal.Set("log: level=trace");
+            BearLib.Terminal.Set("window: title='SquidLibSharp Demo', size=90x30; output: vsync=true");
+            // how to configure a font:
+            //BearLib.Terminal.Set("window: title='SquidLibSharp Demo', size=90x30; font: Rogue-Zodiac-12x24.png, size=12x24, codepage=custom.txt; output: vsync=true");
+            while (!BearLib.Terminal.Check(BearLib.Terminal.TK_ESCAPE)) {
+                BearLib.Terminal.Read();
+                BearLib.Terminal.Put((int)(System.DateTime.UtcNow.Ticks / 151 % 90), (int)(System.DateTime.UtcNow.Ticks / 0xDED % 30), 'a');
+                BearLib.Terminal.Refresh();
+            }
         }
     }
 }
