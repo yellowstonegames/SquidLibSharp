b9809432c37969577fd053128134efe44d6d995f
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-02-18 22:01:02 -0800

    Colors in demo! SColor.cs has a huge list of colors.

--- code/Demo/Main.cs
+++ code/Demo/Main.cs
@@ -13,7 +13,9 @@ namespace Demo {
             Terminal.Open();
             Terminal.Set("log: level=trace");
             int width = 90, height = 30;
+            //Terminal.Set($"window.size={width}x{height};");
             Terminal.Set($"window: title='SquidLibSharp Demo', size={width}x{height}; output: vsync=true; font: Iosevka.ttf, size=9x21, hinting=autohint");
+            SColor.LoadSColor();
             Terminal.Refresh();
             
             // how to configure a font:
@@ -25,6 +27,9 @@ namespace Demo {
                         keepRunning = false;
                         break;
                     case int val:
+                        string name = rng.RandomElement(SColor.Names);
+                        System.Console.WriteLine(name);
+                        Terminal.Color(Terminal.ColorFromName(name));
                         Terminal.Put(rng.NextInt(width), rng.NextInt(height), ArrayTools.LetterAt(val));
                         Terminal.Refresh();
                         break;
