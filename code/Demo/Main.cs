ab1ce434b60bea09be52bd2598ca6a1dcb1f7615
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-02-19 20:25:25 -0800

    Add Aurora colors on their own, fiddle with Main loop to see how fast it is
    With vsync off, it's quite fast!

--- code/Demo/Main.cs
+++ code/Demo/Main.cs
@@ -14,23 +14,32 @@ namespace Demo {
             Terminal.Set("log: level=trace");
             int width = 90, height = 30;
             //Terminal.Set($"window.size={width}x{height};");
-            Terminal.Set($"window: title='SquidLibSharp Demo', size={width}x{height}; output: vsync=true; font: Iosevka.ttf, size=9x21, hinting=autohint");
-            SColor.LoadSColor();
+            Terminal.Set($"window: title='SquidLibSharp Demo', size={width}x{height}; output: vsync=false; font: Iosevka.ttf, size=9x21, hinting=autohint");
+            SColor.LoadAurora();
             Terminal.Refresh();
+            int input = 0;
             while (keepRunning) {
-                switch (Terminal.Read()) {
-                    case Terminal.TK_ESCAPE:
-                    case Terminal.TK_CLOSE:
-                        keepRunning = false;
-                        break;
-                    case int val:
-                        string name = rng.RandomElement(SColor.Names);
-                        System.Console.WriteLine(name);
-                        Terminal.Color(Terminal.ColorFromName(name));
-                        Terminal.Put(rng.NextInt(width), rng.NextInt(height), ArrayTools.LetterAt(val));
-                        Terminal.Refresh();
-                        break;
+                input = Terminal.Peek();
+                if (input == Terminal.TK_Q || input == Terminal.TK_ESCAPE || input == Terminal.TK_CLOSE)
+                    keepRunning = false;
+                else {
+                    if(Terminal.HasInput())
+                        input = Terminal.Read();
+                    Terminal.Color(Terminal.ColorFromName(rng.RandomElement(SColor.AuroraNames)));
+                    Terminal.Put(rng.NextInt(width), rng.NextInt(height), ArrayTools.LetterAt(input));
+                    Terminal.Refresh();
                 }
+                //switch (Terminal.Read()) {
+                //    case Terminal.TK_ESCAPE:
+                //    case Terminal.TK_CLOSE:
+                //        keepRunning = false;
+                //        break;
+                //    case int val:
+                //        Terminal.Color(Terminal.ColorFromName(rng.RandomElement(SColor.AuroraNames)));
+                //        Terminal.Put(rng.NextInt(width), rng.NextInt(height), ArrayTools.LetterAt(rng.NextInt()));
+                //        Terminal.Refresh();
+                //        break;
+                //}
 
             }
         }
