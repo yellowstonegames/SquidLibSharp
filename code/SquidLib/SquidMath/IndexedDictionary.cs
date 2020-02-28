cdda7a0e6df4315f0bc790853a1b4117a0ff6c10
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-02-28 02:34:44 -0800

    Big push to IndexedDictionary; almost done.

--- code/SquidLib/SquidMath/IndexedDictionary.cs
+++ code/SquidLib/SquidMath/IndexedDictionary.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 
 namespace SquidLib.SquidMath {
+    /// <summary>
+    /// Only for use with indexers in IndexedDictionary to show you want to access a key.
+    /// There is only one enum constant here, At.
+    /// </summary>
+    public enum Key {
+        At
+    }
+    /// <summary>
+    /// Only for use with indexers in IndexedDictionary to show you want to access a value.
+    /// There is only one enum constant here, At.
+    /// </summary>
+    public enum Value {
+        At
+    }
+    /// <summary>
+    /// An IDictionary that also allows access to its keys and values in insertion-order, allowing random access.
+    /// </summary>
+    /// <typeparam name="TKey">Key type</typeparam>
+    /// <typeparam name="TValue">Value type</typeparam>
     public class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue> {
 
         /// <summary>
-        ///     Initializes a new instance of the System.Collections.Generic.Dictionary`2 class
+        ///     Initializes a new instance of the SquidLib.SquidMath.IndexedDictionary class
         ///     that is empty, has the default initial capacity, and uses the default equality
         ///     comparer for the key type.
         /// </summary>
@@ -16,18 +34,17 @@ namespace SquidLib.SquidMath {
             Items = new List<TKey>();
         }
         /// <summary>
-        ///     Initializes a new instance of the System.Collections.Generic.Dictionary`2 class
+        ///     Initializes a new instance of the SquidLib.SquidMath.IndexedDictionary class
         ///     that contains elements copied from the specified System.Collections.Generic.IDictionary`2
         ///     and uses the default equality comparer for the key type.
         /// </summary>
-        /// <param name="dictionary">The System.Collections.Generic.IDictionary`2 whose elements are copied to the new System.Collections.Generic.Dictionary`2.</param>
+        /// <param name="dictionary">The System.Collections.Generic.IDictionary`2 whose elements are copied to the new SquidLib.SquidMath.IndexedDictionary.</param>
         public IndexedDictionary(IDictionary<TKey, TValue> dictionary) {
             Dict = new Dictionary<TKey, TValue>(dictionary);
             Items = new List<TKey>(Dict.Keys);
         }
-        ///     
         /// <summary>
-        ///     Initializes a new instance of the System.Collections.Generic.Dictionary`2 class
+        ///     Initializes a new instance of the SquidLib.SquidMath.IndexedDictionary class
         ///     that is empty, has the default initial capacity, and uses the specified System.Collections.Generic.IEqualityComparer`1.
         /// </summary>
         /// <param name="comparer">The System.Collections.Generic.IEqualityComparer`1 implementation to use when comparing keys, or null to use the default System.Collections.Generic.EqualityComparer`1 for the type of the key.</param>
@@ -35,75 +52,68 @@ namespace SquidLib.SquidMath {
             Dict = new Dictionary<TKey, TValue>(comparer);
             Items = new List<TKey>();
         }
-        //
-        // Summary:
-        //     Initializes a new instance of the System.Collections.Generic.Dictionary`2 class
-        //     that is empty, has the specified initial capacity, and uses the default equality
-        //     comparer for the key type.
-        //
-        // Parameters:
-        //   capacity:
-        //     The initial number of elements that the System.Collections.Generic.Dictionary`2
-        //     can contain.
-        //
-        // Exceptions:
-        //   T:System.ArgumentOutOfRangeException:
-        //     capacity is less than 0.
+        /// <summary>
+        /// Initializes a new instance of the SquidLib.SquidMath.IndexedDictionary class
+        /// that is empty, has the specified initial capacity, and uses the default equality
+        /// comparer for the key type.
+        /// </summary>
+        /// <param name="capacity">The initial number of elements that the SquidLib.SquidMath.IndexedDictionary can contain.</param>
         public IndexedDictionary(int capacity) {
             Dict = new Dictionary<TKey, TValue>(capacity);
             Items = new List<TKey>(capacity);
         }
-        //
-        // Summary:
-        //     Initializes a new instance of the System.Collections.Generic.Dictionary`2 class
-        //     that contains elements copied from the specified System.Collections.Generic.IDictionary`2
-        //     and uses the specified System.Collections.Generic.IEqualityComparer`1.
-        //
-        // Parameters:
-        //   dictionary:
-        //     The System.Collections.Generic.IDictionary`2 whose elements are copied to the
-        //     new System.Collections.Generic.Dictionary`2.
-        //
-        //   comparer:
-        //     The System.Collections.Generic.IEqualityComparer`1 implementation to use when
-        //     comparing keys, or null to use the default System.Collections.Generic.EqualityComparer`1
-        //     for the type of the key.
-        //
-        // Exceptions:
-        //   T:System.ArgumentNullException:
-        //     dictionary is null.
-        //
-        //   T:System.ArgumentException:
-        //     dictionary contains one or more duplicate keys.
+        /// <summary>
+        /// Initializes a new instance of the SquidLib.SquidMath.IndexedDictionary class
+        /// that contains elements copied from the specified System.Collections.Generic.IDictionary`2
+        /// and uses the specified System.Collections.Generic.IEqualityComparer`1.
+        /// </summary>
+        /// <param name="dictionary">The System.Collections.Generic.IDictionary`2 whose elements are copied to the new SquidLib.SquidMath.IndexedDictionary.</param>
+        /// <param name="comparer">The System.Collections.Generic.IEqualityComparer`1 implementation to use when comparing keys, or null to use the default System.Collections.Generic.EqualityComparer`1 for the type of the key.</param>
         public IndexedDictionary(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer) {
             Dict = new Dictionary<TKey, TValue>(dictionary, comparer);
             Items = new List<TKey>(Dict.Keys);
         }
-        //
-        // Summary:
-        //     Initializes a new instance of the System.Collections.Generic.Dictionary`2 class
-        //     that is empty, has the specified initial capacity, and uses the specified System.Collections.Generic.IEqualityComparer`1.
-        //
-        // Parameters:
-        //   capacity:
-        //     The initial number of elements that the System.Collections.Generic.Dictionary`2
-        //     can contain.
-        //
-        //   comparer:
-        //     The System.Collections.Generic.IEqualityComparer`1 implementation to use when
-        //     comparing keys, or null to use the default System.Collections.Generic.EqualityComparer`1
-        //     for the type of the key.
-        //
-        // Exceptions:
-        //   T:System.ArgumentOutOfRangeException:
-        //     capacity is less than 0.
+        /// <summary>
+        /// Initializes a new instance of the SquidLib.SquidMath.IndexedDictionary class
+        /// that is empty, has the specified initial capacity, and uses the specified System.Collections.Generic.IEqualityComparer`1.
+        /// </summary>
+        /// <param name="capacity">The initial number of elements that the SquidLib.SquidMath.IndexedDictionary can contain.</param>
+        /// <param name="comparer">The System.Collections.Generic.IEqualityComparer`1 implementation to use when comparing keys, or null to use the default System.Collections.Generic.EqualityComparer`1 for the type of the key.</param>
         public IndexedDictionary(int capacity, IEqualityComparer<TKey> comparer) {
             Dict = new Dictionary<TKey, TValue>(capacity, comparer);
             Items = new List<TKey>(capacity);
         }
 
 
-        public TValue this[TKey key] { get => Dict[key]; set => Dict[key] = value; }
+        public TValue this[TKey key] {
+            get => Dict[key]; set { 
+                if (!Dict.ContainsKey(key)) 
+                    Items.Add(key);
+                Dict[key] = value; }
+        }
+
+        /// <summary>
+        /// Use with <code>MyKey k = myIndexedDictionary[Key.At, 0]</code> or <code>myIndexedDictionary[Key.At, 0] = k;</code>
+        /// </summary>
+        /// <param name="op">Any Key enum, which means just Key.At</param>
+        /// <param name="index">The index into the ordering of the IndexedSet; must be at least 0 and less than Count</param>
+        /// <returns>the TKey at the given index</returns>
+        public TKey this[Key op, int index] {
+            get => Items[index]; set {
+                TValue v = Dict[Items[index]];
+                Dict.Remove(Items[index]);
+                Dict[value] = v;
+            }
+        }
+        /// <summary>
+        /// Use with <code>MyValue v = myIndexedDictionary[Value.At, 0]</code> or <code>myIndexedDictionary[Value.At, 0] = v;</code>
+        /// </summary>
+        /// <param name="op">Any Value enum, which means just Value.At</param>
+        /// <param name="index">The index into the ordering of the IndexedSet; must be at least 0 and less than Count</param>
+        /// <returns>the TValue at the given index</returns>
+        public TValue this[Value op, int index] {
+            get => Dict[Items[index]]; set => Dict[Items[index]] = value;
+        }
 
         public Dictionary<TKey, TValue> Dict { get; private set; }
         public List<TKey> Items { get; private set; }
@@ -137,21 +147,13 @@ namespace SquidLib.SquidMath {
             Items.Clear();
         }
 
-        public bool Contains(KeyValuePair<TKey, TValue> item) {
-            return ((IDictionary<TKey, TValue>)Dict).Contains(item);
-        }
+        public bool Contains(KeyValuePair<TKey, TValue> item) => ((IDictionary<TKey, TValue>)Dict).Contains(item);
 
-        public bool ContainsKey(TKey key) {
-            return Dict.ContainsKey(key);
-        }
+        public bool ContainsKey(TKey key) => Dict.ContainsKey(key);
 
-        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) {
-            ((IDictionary<TKey, TValue>)Dict).CopyTo(array, arrayIndex);
-        }
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => ((IDictionary<TKey, TValue>)Dict).CopyTo(array, arrayIndex);
 
-        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() {
-            return ((IDictionary<TKey, TValue>)Dict).GetEnumerator();
-        }
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => ((IDictionary<TKey, TValue>)Dict).GetEnumerator();
 
         public bool Remove(TKey key) {
             if (Dict.Remove(key)) {
@@ -169,13 +171,9 @@ namespace SquidLib.SquidMath {
             return false;
         }
 
-        public bool TryGetValue(TKey key, out TValue value) {
-            return Dict.TryGetValue(key, out value);
-        }
+        public bool TryGetValue(TKey key, out TValue value) => Dict.TryGetValue(key, out value);
 
-        IEnumerator IEnumerable.GetEnumerator() {
-            return ((IDictionary<TKey, TValue>)Dict).GetEnumerator();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => ((IDictionary<TKey, TValue>)Dict).GetEnumerator();
         public sealed class ValueCollection : ICollection<TValue>, ICollection, IReadOnlyCollection<TValue> {
             private readonly List<TKey> _items;
             private readonly Dictionary<TKey, TValue> _dictionary;
@@ -220,9 +218,7 @@ namespace SquidLib.SquidMath {
             void ICollection<TValue>.Add(TValue item)
                 => throw new NotSupportedException("An IndexedDictionary cannot be modified via its values collection.");
 
-            bool ICollection<TValue>.Remove(TValue item) {
-                throw new NotSupportedException("An IndexedDictionary cannot be modified via its values collection.");
-            }
+            bool ICollection<TValue>.Remove(TValue item) => throw new NotSupportedException("An IndexedDictionary cannot be modified via its values collection.");
 
             void ICollection<TValue>.Clear()
                 => throw new NotSupportedException("An IndexedDictionary cannot be modified via its values collection.");
