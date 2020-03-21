a03ddd4d56c9d525a57f893574f5c63c9b0fee66
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-03-20 23:02:44 -0700

    Add access to 2D Hilbert Curve data (256x256 for now)
    This gets used by one of the map generators in SquidLib.

---
+++ code/SquidLib/SquidMath/HilbertCurve.cs
@@ -0,0 +1,13 @@
+﻿namespace SquidLib.SquidMath {
+    public static class HilbertCurve {
+        public static readonly byte[] Hilbert2DX = Properties.Resources.Hilbert2D_X;
+        public static readonly byte[] Hilbert2DY = Properties.Resources.Hilbert2D_Y;
+        public static readonly byte[] Hilbert2DDist = Properties.Resources.Hilbert2D_Dist;
+
+        public static int Distance(int x, int y) {
+            int idx = (x | y << 8) << 1 & 0x1FFFE;
+            return Hilbert2DDist[idx] | Hilbert2DDist[idx + 1] << 8;
+        }
+        public static Coord Get(int distance) => Coord.Get(Hilbert2DX[distance & 0xFFFF], Hilbert2DY[distance & 0xFFFF]);
+    }
+}
