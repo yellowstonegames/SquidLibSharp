059ab7459d260cdd6ceb1cdf0e8efa079581d5fc
Eben Howard <>
2020-05-27 23:26:09 -0600

    trying out SadConsole

--- code/Rogue Delivery/RogueDelivery.cs
+++ code/Rogue Delivery/RogueDelivery.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using System.Linq;
+
 using BearLib;
 using ColorHelper;
+
+using Microsoft.Xna.Framework;
+using Keys = Microsoft.Xna.Framework.Input.Keys;
+
+using SadConsole;
+using Console = SadConsole.Console;
+using SadConsole.Components;
+
 using SquidLib;
 using SquidLib.SquidGrid;
 using SquidLib.SquidMath;
+using SadConsole.Input;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.AccessControl;
 
 namespace RogueDelivery {
     class RogueDelivery {
         private enum ControlType { OnFoot, DrivingWagon }
+        private enum OutputType { BearLibTerminal, SadConsole }
 
         private bool keepRunning = true;
         private RNG rng;
@@ -29,6 +42,7 @@ namespace RogueDelivery {
 
         private Dictionary<Coord, char> borders = new Dictionary<Coord, char>();
 
+        private OutputType outputType;
         private ControlType controlType = ControlType.OnFoot;
         private Mob player;
         private Physical playerPhysical;
@@ -38,12 +52,13 @@ namespace RogueDelivery {
         private List<Mob> littleMobs = new List<Mob>();
 
         static void Main() {
-            var rd = new RogueDelivery(); // start up the primary system
+            var rd = new RogueDelivery(OutputType.SadConsole); // start up the primary system
+            rd.InitObjects();
             rd.Start();
         }
 
         private void InitObjects() {
-            player = new Mob { Rep = new Representation { Glyph = '@', Color = Color.Aquamarine } };
+            player = new Mob { Rep = new Representation { Glyph = '@', Color = System.Drawing.Color.Aquamarine } };
             playerPhysical = new Physical() {
                 Name = "Rogue",
                 Class = "Delivery Driver",
@@ -63,21 +78,12 @@ namespace RogueDelivery {
                     Facing = rng.RandomElement(DirectionTools.Cardinals)
                 });
             }
-        }
 
-        private RogueDelivery() {
-            rng = new RNG(DateTime.Today.ToString(CultureInfo.InvariantCulture)); // Different seed every day, but the same seed on that day, for testing.
-
-            width = windowWidth - 2;
-            height = windowHeight - logHeight - statusHeight - 4;
-
-            InitObjects();
-
-            for(int x = 0; x < width; x++) {
-                for(int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
                     byte blue = BlueNoise.GetSeeded(x, y, 1234567u);
                     if (blue < 7) {
-                        littleMobs.Add(new Mob(Prototype.Pebble) { Location = Coord.Get(x,y) });
+                        littleMobs.Add(new Mob(Prototype.Pebble) { Location = Coord.Get(x, y) });
                     } else if (blue < 30) {
                         littleMobs.Add(new Mob(Prototype.Bush) { Location = Coord.Get(x, y) });
                     } else {
@@ -89,7 +95,16 @@ namespace RogueDelivery {
             // TODO - check that space is clear enough for them to get placed
             wagon.Location = Coord.Get(width / 2, height / 2);
             player.Location = Coord.Get(wagon.Location.X - 4, wagon.Location.Y); ;
+        }
 
+        private RogueDelivery(OutputType outputType) {
+            this.outputType = outputType;
+            rng = new RNG(DateTime.Today.ToString(CultureInfo.InvariantCulture)); // Different seed every day, but the same seed on that day, for testing.
+            width = windowWidth - 2;
+            height = windowHeight - logHeight - statusHeight - 4;
+        }
+
+        private void InitDisplay() {
             // init display style
             for (int x = 1; x < windowWidth - 1; x++) {
                 borders[Coord.Get(x, 0)] = '─';
@@ -111,66 +126,117 @@ namespace RogueDelivery {
             borders[Coord.Get(windowWidth - 1, windowHeight - 1 - statusHeight - 1 - logHeight - 1)] = '┤';
 
             // Setup terminal
-            Terminal.Open();
-            Terminal.Set(
-                $"window: title='Rogue Delivery', size={windowWidth}x{windowHeight};" +
-                $"output: vsync=true;" +
-                $"font: Iosevka.ttf, size=9x21, hinting=autohint"
-                );
-            BltColor.LoadAurora();
-            Terminal.Color(rng.RandomElement<string>(BltColor.AuroraNames));
-            Terminal.Refresh();
+            switch (outputType) {
+                case OutputType.BearLibTerminal:
+                    Terminal.Open();
+                    Terminal.Set(
+                        $"window: title='Rogue Delivery', size={windowWidth}x{windowHeight};" +
+                        $"output: vsync=true;" +
+                        $"font: Iosevka.ttf, size=9x21, hinting=autohint"
+                        );
+                    BltColor.LoadAurora();
+                    Terminal.Color(rng.RandomElement<string>(BltColor.AuroraNames));
+                    Terminal.Refresh();
+                    break;
+                case OutputType.SadConsole:
+                    SadConsole.Game.Create(windowWidth, windowHeight);
+                    SadConsole.Game.OnInitialize = () => {
+                        var console = new Console(windowWidth, windowHeight);
+                        console.IsFocused = true;
+                        console.Cursor.IsEnabled = false;
+                        console.Components.Add(new SadKeyboardController(this));
+
+                        SadConsole.Global.CurrentScreen = console;
+                    };
+                    SadConsole.Game.OnUpdate = (gameTime) => DrawMap();
+                    break;
+            }
         }
 
         private void Start() {
-            DrawMap();
-            Terminal.Refresh();
+            InitDisplay();
 
-            while (keepRunning) {
-                if (Terminal.HasInput()) {
-                    int read = Terminal.Read();
-                    switch (read) {
-                        case Terminal.TK_ESCAPE:
-                        case Terminal.TK_CLOSE:
-                            keepRunning = false;
-                            break;
-                        case Terminal.TK_LEFT:
-                            MovePlayer(Direction.Left);
-                            break;
-                        case Terminal.TK_UP:
-                            MovePlayer(Direction.Up);
-                            break;
-                        case Terminal.TK_DOWN:
-                            MovePlayer(Direction.Down);
-                            break;
-                        case Terminal.TK_RIGHT:
-                            MovePlayer(Direction.Right);
-                            break;
-                        case Terminal.TK_SPACE when controlType == ControlType.DrivingWagon:
-                            controlType = ControlType.OnFoot;
-                            bigMobs.Add(wagon);
-                            break;
-                        case Terminal.TK_SPACE when controlType == ControlType.OnFoot:
-                            controlType = ControlType.DrivingWagon;
-                            bigMobs.Remove(wagon);
-                            break;
-                        default:
-                            // ignore unknown commands
-                            Terminal.Color(Color.White);
-                            Message($"Pressed {(char)Terminal.State(Terminal.TK_WCHAR)}");
-                            break;
-                    }
+            switch (outputType) {
+                case OutputType.BearLibTerminal:
                     DrawMap();
                     Terminal.Refresh();
-                }
+
+                    while (keepRunning) {
+                        if (Terminal.HasInput()) {
+                            int read = Terminal.Read();
+                            switch (read) {
+                                case Terminal.TK_ESCAPE:
+                                case Terminal.TK_CLOSE:
+                                    keepRunning = false;
+                                    break;
+                                case Terminal.TK_LEFT:
+                                    MovePlayer(Direction.Left, Terminal.Check(Terminal.TK_SHIFT));
+                                    break;
+                                case Terminal.TK_UP:
+                                    MovePlayer(Direction.Up, Terminal.Check(Terminal.TK_SHIFT));
+                                    break;
+                                case Terminal.TK_DOWN:
+                                    MovePlayer(Direction.Down, Terminal.Check(Terminal.TK_SHIFT));
+                                    break;
+                                case Terminal.TK_RIGHT:
+                                    MovePlayer(Direction.Right, Terminal.Check(Terminal.TK_SHIFT));
+                                    break;
+                                case Terminal.TK_SPACE when controlType == ControlType.DrivingWagon:
+                                    controlType = ControlType.OnFoot;
+                                    bigMobs.Add(wagon);
+                                    break;
+                                case Terminal.TK_SPACE when controlType == ControlType.OnFoot:
+                                    controlType = ControlType.DrivingWagon;
+                                    bigMobs.Remove(wagon);
+                                    break;
+                                default:
+                                    // ignore unknown commands
+                                    Terminal.Color(System.Drawing.Color.White);
+                                    Message($"Pressed {(char)Terminal.State(Terminal.TK_WCHAR)}");
+                                    break;
+                            }
+                            DrawMap();
+                            Terminal.Refresh();
+                        }
+                    }
+                    Terminal.Close();
+                    break;
+                case OutputType.SadConsole:
+                    SadConsole.Game.Instance.Run();
+                    SadConsole.Game.Instance.Dispose();
+                    break;
             }
-            Terminal.Close();
         }
 
-        private void MovePlayer(Direction direction) {
+        private class SadKeyboardController : KeyboardConsoleComponent {
+            private RogueDelivery master;
+            public SadKeyboardController(RogueDelivery master) => this.master = master;
+
+            // TODO - queue multiple "simultaneous" inputs registered
+            public override void ProcessKeyboard(Console console, Keyboard info, out bool handled) {
+                handled = false;
+
+                // check for movements
+                if (info.IsKeyPressed(Keys.Left)) {
+                    master.MovePlayer(Direction.Left, info.IsKeyDown(Keys.LeftShift) || info.IsKeyDown(Keys.RightShift));
+                    handled = true;
+                } else if (info.IsKeyPressed(Keys.Right)) {
+                    master.MovePlayer(Direction.Right, info.IsKeyDown(Keys.LeftShift) || info.IsKeyDown(Keys.RightShift));
+                    handled = true;
+                } else if (info.IsKeyPressed(Keys.Up)) {
+                    master.MovePlayer(Direction.Up, info.IsKeyDown(Keys.LeftShift) || info.IsKeyDown(Keys.RightShift));
+                    handled = true;
+                } else if (info.IsKeyPressed(Keys.Down)) {
+                    master.MovePlayer(Direction.Down, info.IsKeyDown(Keys.LeftShift) || info.IsKeyDown(Keys.RightShift));
+                    handled = true;
+                }
+            }
+        }
+
+        private void MovePlayer(Direction direction, bool strafe = false) {
             switch (controlType) {
                 case ControlType.DrivingWagon:
-                    if (wagon.Facing == direction || Terminal.Check(Terminal.TK_SHIFT)) {
+                    if (wagon.Facing == direction || strafe) {
                         if (IsBlocked(wagon, wagon.Facing, direction)) {
                             //
                         } else {
@@ -213,8 +279,19 @@ namespace RogueDelivery {
                         .Select(c => c + bm.DrawingOffset()))))
                 .Any();
 
+        private void ClearArea(int x, int y, int clearWidth, int clearHeight) {
+            switch (outputType) {
+                case OutputType.BearLibTerminal:
+                    Terminal.ClearArea(x, y, clearWidth, clearHeight);
+                    break;
+                case OutputType.SadConsole:
+                    SadConsole.Global.CurrentScreen.Clear(new Microsoft.Xna.Framework.Rectangle(x, y, clearWidth, clearHeight));
+                    break;
+            }
+        }
+
         private void DrawMap() {
-            Terminal.ClearArea(1, 1, width, height);
+            ClearArea(1, 1, width, height);
 
             foreach (Mob mob in littleMobs) {
                 PutOnMap(mob);
@@ -235,29 +312,51 @@ namespace RogueDelivery {
                     break;
             }
 
-            Terminal.Color(rng.RandomElement(BltColor.AuroraNames));
+            SetColor(GetRandomColor());
             PaintBorder();
 
             // Status section
             int startY = windowHeight - statusHeight - 1;
-            Terminal.Color(player.Rep.Color);
+            SetColor(player.Rep.Color);
             int startX = Put(2, startY, playerPhysical.Name);
-            Terminal.Color(Color.White);
+            SetColor(System.Drawing.Color.White);
             startX = Put(startX + 1, startY, "The");
-            Terminal.Color(Color.LightCoral);
+            SetColor(System.Drawing.Color.LightCoral);
             startX = Put(startX + 1, startY, playerPhysical.Class);
-            Terminal.Color(Color.White);
+            SetColor(System.Drawing.Color.White);
             startX = Put(startX + 1, startY, "--");
-            Terminal.Color(Color.Silver);
+            SetColor(System.Drawing.Color.Silver);
             startX = Put(startX + 1, startY, "Strength: " + playerPhysical.Strength);
-            Terminal.Color(Color.HotPink);
+            SetColor(System.Drawing.Color.HotPink);
             startX = Put(startX + 2, startY, "Luck: " + playerPhysical.Luck);
-            Terminal.Color(Color.MediumPurple);
+            SetColor(System.Drawing.Color.MediumPurple);
             startX = Put(startX + 2, startY, "Wiles: " + playerPhysical.Wiles);
-            Terminal.Color(Color.RosyBrown);
+            SetColor(System.Drawing.Color.RosyBrown);
             startX = Put(startX + 2, startY, "XP: " + playerPhysical.XP);
-            Terminal.Color(Color.SeaGreen);
+            SetColor(System.Drawing.Color.SeaGreen);
             startX = Put(startX + 2, startY, "Health: " + playerPhysical.Health);
+        }
+
+        private System.Drawing.Color GetRandomColor() {
+            switch (outputType) {
+                case OutputType.BearLibTerminal:
+                    return Terminal.ColorFromName(rng.RandomElement(BltColor.AuroraNames));
+                case OutputType.SadConsole:
+                    return System.Drawing.Color.FromArgb((int)Microsoft.Xna.Framework.Color.White.GetRandomColor(rng).ToInteger());
+                default:
+                    return System.Drawing.Color.Turquoise;
+            }
+        }
+
+        private void SetColor(System.Drawing.Color color) {
+            switch (outputType) {
+                case OutputType.BearLibTerminal:
+                    Terminal.Color(color);
+                    break;
+                case OutputType.SadConsole:
+                    SadConsole.Global.CurrentScreen.DefaultForeground = new Microsoft.Xna.Framework.Color((uint)color.ToArgb());
+                    break;
+            }
         }
 
         private void PaintBorder() {
@@ -271,28 +370,46 @@ namespace RogueDelivery {
                 log[i - 1] = log[i];
             }
             log[log.Length] = message;
-            Terminal.ClearArea(1, height + 3, width, logHeight);
+            ClearArea(1, height + 3, width, logHeight);
             for (int i = 0; i < log.Length; i++) {
-                Terminal.Print(1, height + 3 + i, log[i]);
+                Put(1, height + 3 + i, log[i]);
             }
         }
 
-        private static void Put(Coord coord, char c) => Terminal.Put(coord.X, coord.Y, c);
+        private void Put(int x, int y, char c) {
+            switch (outputType) {
+                case OutputType.BearLibTerminal:
+                    Terminal.Put(x, y, c);
+                    break;
+                case OutputType.SadConsole:
+                    SadConsole.Global.CurrentScreen.SetGlyph(x, y, c);
+                    break;
+            }
+        }
 
-        private static int Put(int x, int y, string s) {
-            Terminal.Print(x, y, s);
+        private void Put(Coord coord, char c) => Put(coord.X, coord.Y, c);
+
+        private int Put(int x, int y, string s) {
+            switch (outputType) {
+                case OutputType.BearLibTerminal:
+                    Terminal.Print(x, y, s);
+                    break;
+                case OutputType.SadConsole:
+                    SadConsole.Global.CurrentScreen.Print(x, y, s);
+                    break;
+            }
             return x + s.Length;
         }
 
         private void PutOnMap(Mob mob) {
-            Terminal.Color(mob.Rep.Color);
+            SetColor(mob.Rep.Color);
             PutOnMap(mob.Location, mob.Rep.Glyph);
         }
 
         private void PutOnMap(BigMob bigMob) {
             Coord drawingOffset = bigMob.DrawingOffset();
             foreach (var (coord, rep) in bigMob.Reps[bigMob.Facing].Tiles) {
-                Terminal.Color(rep.Color);
+                SetColor(rep.Color);
                 PutOnMap(drawingOffset + coord, rep.Glyph);
             }
         }
@@ -311,7 +428,7 @@ namespace RogueDelivery {
         /// <param name="c"></param>
         private void PutOnMap(int x, int y, char c) {
             if (InMapBounds(x, y)) {
-                Terminal.Put(x + 1, y + 1, c); // handle window offset
+                Put(x + 1, y + 1, c); // handle window offset
             }
         }
 
