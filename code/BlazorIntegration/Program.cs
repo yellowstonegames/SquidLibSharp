bd81c88433161c0779b93f6a6558a75f859025dd
Eben Howard <howard@squidpony.com>
2020-06-07 19:54:08 -0600

    adjustments to prep for github pages use

--- code/BlazorIntegration/Program.cs
+++ code/BlazorIntegration/Program.cs
@@ -19,4 +19,11 @@ namespace BlazorIntegration {
             await builder.Build().RunAsync();
         }
     }
+
+    public static class KeyValuePairExtansions {
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> kvp, out TKey key, out TValue value) {
+            key = kvp.Key;
+            value = kvp.Value;
+        }
+    }
 }
