e4d91d7285746d85718e03e99e4c033d0031ecc9
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-07-30 19:59:59 -0700

    Print the sample language output to a temp file; keep one in Test/

--- code/Test/LanguageTest.cs
+++ code/Test/LanguageTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
-
+using System.IO;
+using System.Text;
 using SquidLib.SquidMath;
 using SquidLib.SquidText;
 
@@ -19,16 +20,35 @@ namespace Test {
             for (int i = 1; i < registered; i++) {
                 LanguageGen lang = LanguageGen.Registry[Value.At, i];
                 Console.WriteLine("===============================================================================");
-                Console.WriteLine(lang.Name.ToUpperInvariant());
+                Console.WriteLine(lang.Name);
+                Console.WriteLine();
                 for (int j = 0; j < 10; j++) {
                     Console.WriteLine(lang.Sentence(random, 1, 30, 79));
                 }
             }
         }
+        public void WriteSentences() {
+            RNG random = new RNG("Language!");
+            int registered = LanguageGen.Registry.Count;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < registered; i++) {
+                LanguageGen lang = LanguageGen.Registry[Value.At, i];
+                sb.AppendLine("===============================================================================");
+                sb.AppendLine(lang.Name);
+                sb.AppendLine();
+                for (int j = 0; j < 10; j++) {
+                    sb.AppendLine(lang.Sentence(random, 1, 30, 79));
+                }
+            }
+            string temp = System.IO.Path.GetTempFileName();
+            File.WriteAllText(temp, sb.ToString());
+            Console.WriteLine(temp);
+        }
         static void Main(string[] _) {
             LanguageTest test = new LanguageTest();
             //test.TestSupportedLanguages();
             test.TestSentences();
+            test.WriteSentences();
         }
     }
 }
