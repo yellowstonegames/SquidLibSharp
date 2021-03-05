91c8b457fda3d457c054685dca047d43404c4eaa
Eben Howard <SquidPony@users.noreply.github.com>
2021-03-04 21:09:32 -0700

    work towards OpenTK usability

---
+++ code/OpentTKIntegration/Components/MainWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Desktop;
+
+namespace OpentTKIntegration.Components {
+    public sealed class MainWindow : GameWindow {
+
+        public MainWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings) {
+        }
+    }
+}
---
+++ code/OpentTKIntegration/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using OpenTK.Windowing.Common.Input;
+
+using OpentTKIntegration.Components;
+
+namespace OpentTKIntegration {
+    class Program {
+        static void Main(string[] args) {
+            var gameSettings = new OpenTK.Windowing.Desktop.GameWindowSettings {
+                RenderFrequency = 60,
+                UpdateFrequency = 60
+            };
+
+            var nativeSettings = new OpenTK.Windowing.Desktop.NativeWindowSettings {
+                Size = new() { X = 500, Y = 500 },
+                API = OpenTK.Windowing.Common.ContextAPI.OpenGL,
+                Title = "SquidLib Sharp OpenTK Demo",
+
+            };
+            new MainWindow(gameSettings, nativeSettings).Run();
+        }
+    }
+}
